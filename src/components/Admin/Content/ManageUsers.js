54bd5aaa1d421b6f8e3a25aae1594663b98cd9a3
VU NGUYEN <bopvip2002@gmail.com>
2025-02-28 00:00:39 +0700

    cm

--- src/components/Admin/Content/ManageUsers.js
+++ src/components/Admin/Content/ManageUsers.js
@@ -1,20 +1,31 @@
-import ModalCreateUser from "./ModalCreateUser";
-
-function ManageUsers(props) {
-  return (
-    <div className="manage-users-container">
-      <div className="title">Manage Users</div>
-      <div className="users-content">
-        <div>
-          <button>Add New Users</button>
+  import ModalCreateUser from "./ModalCreateUser";
+  import "./ManageUsers.scss";
+  import { faPlus } from "@fortawesome/free-solid-svg-icons";
+  import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+  import { useState } from "react";
+  function ManageUsers(props) {
+    const [shoModalCreateUser, setShoModalCreateUser] = useState(false);
+    return (
+      <div className="manage-users-container">
+        <div className="title">Manage Users</div>
+        <div className="users-content">
+          <div className="btn-add-new">
+            <button
+              className="btn btn-primary"
+              onClick={() => setShoModalCreateUser(true)}
+            >
+              <FontAwesomeIcon icon={faPlus} style={{ marginRight: 5 }} />
+              Add New Users
+            </button>
+          </div>
+          <div className="table-users-container">Table Users</div>
         </div>
-        <div>
-          Table Users
-          <ModalCreateUser />
-        </div>
+        <ModalCreateUser
+          show={shoModalCreateUser}
+          setShow={setShoModalCreateUser}
+        />
       </div>
-    </div>
-  );
-}
+    );
+  }
 
-export default ManageUsers;
+  export default ManageUsers;
--- src/components/Admin/Content/ModalCreateUser.js
+++ src/components/Admin/Content/ModalCreateUser.js
@@ -1,20 +1,74 @@
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import axios from "axios";
 
-function ModalCreateUser() {
-  const [show, setShow] = useState(false);
+function ModalCreateUser(props) {
+  const { show, setShow } = props;
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const handleClose = () => {
+    setShow(false);
+    setEmail("");
+    setPassword("");
+
+    setRole("USER");
+    setImage("");
+    setPreviewImage("");
+    setUsername("");
+  };
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [role, setRole] = useState("USER");
+  const [image, setImage] = useState("");
+  const [previewImage, setPreviewImage] = useState("");
+
+  const handleUploadImage = (e) => {
+    if (e.target && e.target.files && e.target.files[0]) {
+      setPreviewImage(URL.createObjectURL(e.target.files[0]));
+      setImage(e.target.files[0]);
+    }
+  };
+
+  const handleSubmitCreateUsers = async () => {
+    // let data = {
+    //   email: email,
+    //   password: password,
+    //   username: username,
+    //   role: role,
+    //   userImage: image,
+    // };
+    // console.log(data);
+    const data = new FormData();
+    data.append("email", email);
+    data.append("password", password);
+    data.append("username", username);
+    data.append("role", role);
+    data.append("userImage", image);
+
+    let res = await axios.post(
+      "http://localhost:8081/api/v1/participant",
+      data
+    );
+    console.log(res);
+  };
 
   return (
     <>
-      <Button variant="primary" onClick={handleShow}>
+      {/* <Button variant="primary" onClick={handleShow}>
         Launch demo modal
-      </Button>
+      </Button> */}
 
-      <Modal show={show} onHide={handleClose} size="lg" backdrop="static">
+      <Modal
+        show={show}
+        onHide={handleClose}
+        size="lg"
+        backdrop="static"
+        className="modal-add-users"
+      >
         <Modal.Header closeButton>
           <Modal.Title>Add New Users</Modal.Title>
         </Modal.Header>
@@ -22,30 +76,64 @@ function ModalCreateUser() {
           <form className="row g-3">
             <div className="col-md-6">
               <label className="form-label">Email</label>
-              <input type="email" className="form-control" />
+              <input
+                type="email"
+                className="form-control"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
             <div className="col-md-6">
               <label className="form-label">Password</label>
-              <input type="password" className="form-control" />
+              <input
+                type="password"
+                className="form-control"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </div>
 
             <div className="col-md-6">
               <label className="form-label">Username</label>
-              <input type="text" className="form-control" />
+              <input
+                type="text"
+                className="form-control"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
             </div>
             <div className="col-md-4">
-              <label className="form-label">Role</label>
+              <label
+                className="form-label"
+                onChange={(e) => setRole(e.target.value)}
+              >
+                Role
+              </label>
               <select className="form-select">
-                <option selected>Choose...</option>
-                <option selected value="USER">
-                  USER
-                </option>
+                <option>Choose...</option>
+                <option value="USER">USER</option>
                 <option value="ADMIN">ADMIN</option>
               </select>
             </div>
+
             <div className="col-md-12">
-              <label className="form-label">Image</label>
-              <input type="file"></input>
+              <label className="form-label label-upload" htmlFor="labelUpload">
+                <FontAwesomeIcon icon={faPlus} />
+                Upload File Image
+              </label>
+              <input
+                type="file"
+                id="labelUpload"
+                hidden
+                onChange={(e) => handleUploadImage(e)}
+              ></input>
+            </div>
+            <div className="col-md-12 img-preview">
+              {previewImage ? (
+                <img src={previewImage} />
+              ) : (
+                <span>Preview Image</span>
+              )}
             </div>
           </form>
         </Modal.Body>
@@ -53,7 +141,7 @@ function ModalCreateUser() {
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary" onClick={handleClose}>
+          <Button variant="primary" onClick={() => handleSubmitCreateUsers()}>
             Save
           </Button>
         </Modal.Footer>
